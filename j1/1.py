1a129af45796147d8754341360189869f2a02da1
Shayan-02 <upm200@gmail.com>
2023-11-27 10:34:03 +0330

    j1

---
+++ j1/1.py
@@ -0,0 +1,21 @@
+a = 1
+b = 0
+x = 3.4
+v = 4
+fname= 'ali'
+lname= "mohammadi"
+d = True
+x = False
+e = not d
+f = not e
+g = bool(b)
+print(g)
+h = 5
+k = bool(h)
+print(k)
+# -----------------------
+z = x + v
+fullname = fname+" "+ lname
+print(fname, lname, sep="*", end="\n\n")
+print(fname, lname)
+fname = fname * 3
---
+++ j1/tamrin.py
@@ -0,0 +1,7 @@
+fn = input("enter the first number: ")
+sn = int(input("enter the second number: "))
+
+# print(fn , sn)
+# print(fn - sn)
+print(fn * sn)
+# print(fn / sn)
