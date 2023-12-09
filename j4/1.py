cac6f38d6760974b3c2fbd34082637a3f53a5caf
Shayan Shahbazi <upm200@gmail.com>
2023-12-10 01:11:53 +0330

    j4

---
+++ j4/1.py
@@ -0,0 +1,29 @@
+
+# for _ in range(1, 4):
+#     a = int(input(f"n{_}: "))
+#     _ += 1
+#     if a in range(1, 11):
+#         print("you passed")
+#         break
+#     else:
+#         print("you failed")
+
+def sayHello():
+    print("hi")
+sayHello()
+
+def sayHello2(name):
+    print("hi", name)
+
+n = input("enter your name: ")
+
+sayHello2(n)
+
+def sums(a, b):
+    c = a + b
+    print(c)
+
+
+c = + 2
+print(c)
+sums(2, 3)
